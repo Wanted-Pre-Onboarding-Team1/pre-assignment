e9db9c964648be77fa457e290511812f00737c40
이지호 <cctv1998@naver.com>
2022-07-05 13:17:38 +0900

    Feat: useObserverFeed 피드에 적용

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import useFeedLoad from '../hooks/useFeedLoad';
 import FeedSection from '../component/feed/FeedSection';
+import useObserverFeed from '../hooks/useObserverFeed';
 
 const FeedPage = () => {
   const { feeds } = useFeedLoad();
+  const { ref } = useObserverFeed(useFeedLoad);
   return (
     <FeedsWrapper>
       {feeds?.map((feed, index) => (
-        <FeedSection feedData={feed} key={`${feed.id}_${index}`} />
+        <FeedSection feedData={feed} key={`${feed.id}_${index}`} ref={ref} />
       ))}
     </FeedsWrapper>
   );
