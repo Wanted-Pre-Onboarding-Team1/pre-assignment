0d8bf166972896be8346e9f09c732b46e3163d01
이지호 <cctv1998@naver.com>
2022-07-05 13:16:41 +0900

    refactor: 사용하지 않는 import 제거

--- src/component/feed/FeedSection.jsx
+++ src/component/feed/FeedSection.jsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import { FiMoreHorizontal, FiSave } from 'react-icons/fi';
+import React from 'react';
+import { FiMoreHorizontal } from 'react-icons/fi';
 import styled from 'styled-components';
 import useLazyCheck from '../../hooks/useLazyCheck';
 import { FlexBox } from '../../styles/commomComponents';
